b4cb5d61ca562d9dfe1bc40e92e5b8fdadece740
teppIu <satsteo@gmail.com>
2022-07-27 07:35:17 +0500

    Initial Commit

---
+++ HomeWorkOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkOne
+//
+//  Created by Stefan Iurin on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
